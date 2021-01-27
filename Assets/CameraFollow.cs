1b1b83248cc459734694372c5ab86a3da403a419
Benjamin Patoizeau <benjamin.patoizeau@cpe.fr>
2021-01-27 16:29:55 +0100

    Camera follows player

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    void LateUpdate()
+    {
+        transform.position = target.position + offset;
+
+    }
+}
