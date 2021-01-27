451e262d337c1b5858ca4f64e37eb792ba4f34a6
Benjamin Patoizeau <benjamin.patoizeau@cpe.fr>
2021-01-27 16:38:08 +0100

    Smooth camera

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -3,13 +3,16 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
+    public Transform player;
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
-
-    void LateUpdate()
+    
+    void FixedUpdate()
     {
-        transform.position = target.position + offset;
+        Vector3 desiredPosition = player.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
 
+        transform.LookAt(player); 
     }
 }
