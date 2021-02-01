01ca902ae614671f82fe410b708f555144b6c5a8
Nadjet Tobbal <nadjetobbal@gmail.com>
2021-02-01 16:12:51 +0100

    ST-2 following func added (in testing)

---
+++ Assets/Scripts/ST-2 Scripts/ST_Movements.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ST_Movements : MonoBehaviour
+{
+    public Transform player;
+    public float followSharpness = 0.1f;
+
+    Vector3 _followOffset;
+
+    void Start()
+    {
+        // Cache the initial offset at time of load/spawn:
+        _followOffset = transform.position - player.position;
+    }
+
+    void LateUpdate()
+    {
+        // Apply that offset to get a target position.
+        Vector3 targetPosition = player.position + _followOffset;
+
+        // Keep our y position unchanged.
+        targetPosition.y = transform.position.y;
+
+        // Smooth follow.    
+        transform.position += (targetPosition - transform.position) * followSharpness;
+    }
+}
