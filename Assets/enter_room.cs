36df7c642a59baeaa51737776a29fedad76d310b
kheb_chaled <mehdixbadaoui@outlook.fr>
2021-02-01 14:01:12 +0100

    can look inside building when entering

---
+++ Assets/enter_room.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enter_room : MonoBehaviour
+{
+    //private Collider collider;
+    public GameObject facade;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("COllision");
+        if (col.tag == "uni") facade.SetActive(!facade.active);
+    }
+
+    //private void OnCollisionExit(Collision col)
+    //{
+    //    Debug.Log("COllision");
+    //    if (col.collider.tag == "player") facade.SetActive(false);
+    //}
+}
