dd2af6b1857f243f2ef1a4f3e8118a1e7299a25e
Benjamin Patoizeau <benjamin.patoizeau@cpe.fr>
2021-02-01 18:41:38 +0100

    Crouch and Jump

---
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/CrouchScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrouchScript : MonoBehaviour
+{
+    private Rigidbody _body;
+    private CapsuleCollider capsuleCollider;
+    public KeyCode Crouch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _body = GetComponent<Rigidbody>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(Crouch))
+        {
+            capsuleCollider.height = 0.5f;
+            capsuleCollider.center = new Vector3(0, 0.13f, 0.06f); 
+        }
+        else
+        {
+            capsuleCollider.height = 1.5f;
+            capsuleCollider.center = new Vector3(0, 0.66f, 0.06f);
+        }
+            
+
+    }
+}
