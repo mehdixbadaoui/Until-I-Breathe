f6ae9aa8443cb7b91eec0c6967eabe9ae161d050
Benjamin Patoizeau <benjamin.patoizeau@cpe.fr>
2021-02-10 20:32:20 +0100

    LedgeClimbing almost done

---
+++ Assets/Scripts/UNI/Ledge.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ledge : MonoBehaviour
+{
+    public float hangingHorizontalOffset;
+    public float hangingVerticalOffset;
+}
---
+++ Assets/Scripts/UNI/LedgeLocator.cs
@@ -0,0 +1,172 @@
+/*This script is part of a larger solution, but can be used as a standalone
+ * script; if you want to use this as a standalone script, make sure everything
+ * that is commented off in psuedocode is no longer commented out, and that
+ * you delete the Initialization method found within this script, as well as
+ * change every reference of character.grabbingLedge to just grabbingLedge.
+ 
+   If you do want to use this with my larger solution, then you don't need to 
+   make any changes to this script
+*/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LedgeLocator : MonoBehaviour
+{
+    public AnimationClip clip;
+    public float climbingHorizontalOffset;
+
+    private Vector3 topOfPlayer;
+    private GameObject ledge;
+    private float animationTime = .5f;
+    private bool falling;
+    private bool moved;
+
+
+    [HideInInspector]
+    public bool grabbingLedge;
+    private Collider col;
+    private Rigidbody rb;
+    private Animator anim;
+
+    private void Start()
+    {
+        col = GetComponent<Collider>();
+        rb =  GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        if (clip != null)
+        {
+            animationTime = clip.length;
+        }
+    }
+
+
+    //protected override void Initializtion()
+    //{
+    //    base.Initializtion();
+    //    //if(clip != null)
+    //    //{
+    //    //    animationTime = clip.length;
+    //    //}
+    //}
+
+    protected virtual void FixedUpdate()
+    {
+        CheckForLedge();
+        LedgeHanging();
+    }
+
+    protected virtual void CheckForLedge()
+    {
+        if (!falling)
+        {
+            if (transform.localScale.x > 0)
+            {
+                topOfPlayer = new Vector3(0, col.bounds.max.y, col.bounds.max.z + .1f);
+                RaycastHit hit; 
+                if (Physics.Raycast(topOfPlayer, transform.TransformDirection(Vector3.forward), out hit, 1f) && hit.collider.GetComponent<Ledge>())
+                {
+                    ledge = hit.collider.gameObject;
+                    if (col.bounds.max.y < ledge.GetComponent<Collider>().bounds.max.y && col.bounds.max.y > ledge.GetComponent<Collider>().bounds.center.y)
+                    {
+                        grabbingLedge = true;
+                        //anim.SetBool("LedgeHanging", true);
+                    }
+                }
+            }
+            else
+            {
+                topOfPlayer = new Vector3(0, col.bounds.max.y, col.bounds.min.z - .1f);
+
+                RaycastHit hit;
+                if (Physics.Raycast(topOfPlayer, transform.TransformDirection(Vector3.back), out hit, 1f) && hit.collider.GetComponent<Ledge>())
+                {
+                    ledge = hit.collider.gameObject;
+                    if (col.bounds.max.y < ledge.GetComponent<Collider>().bounds.max.y && col.bounds.max.y > ledge.GetComponent<Collider>().bounds.center.y)
+                    {
+                        //anim.SetBool("LedgeHanging", true);
+                        grabbingLedge = true;
+                    }
+                }
+            }
+            if (ledge != null && grabbingLedge)
+            {
+                AdjustPlayerPosition();
+                rb.velocity = Vector3.zero;
+                rb.isKinematic = true; 
+                GetComponent<alt_mvt>().enabled = false;
+            }
+            else
+            {
+                rb.isKinematic = false;
+                GetComponent<alt_mvt>().enabled = true;
+            }
+        }
+    }
+
+    protected virtual void LedgeHanging()
+    {
+        if (grabbingLedge && Input.GetAxis("Vertical") > 0)
+        {
+            //anim.SetBool("LedgeHanging", false);
+            if (transform.localScale.x > 0)
+            {
+                StartCoroutine(ClimbingLedge(new Vector3(0, ledge.GetComponent<Collider2D>().bounds.max.y + col.bounds.extents.y, transform.position.z + climbingHorizontalOffset), animationTime - .3f));
+            }
+            else
+            {
+                StartCoroutine(ClimbingLedge(new Vector3(0, ledge.GetComponent<Collider2D>().bounds.max.y + col.bounds.extents.y, transform.position.z - climbingHorizontalOffset), animationTime - .3f));
+            }
+        }
+        if (grabbingLedge && Input.GetAxis("Vertical") < 0)
+        {
+            ledge = null;
+            moved = false;
+            anim.SetBool("LedgeHanging", false);
+            falling = true;
+            rb.isKinematic = false;
+            GetComponent<alt_mvt>().enabled = true;
+            Invoke("NotFalling", .5f);
+        }
+    }
+
+    protected virtual IEnumerator ClimbingLedge(Vector3 topOfPlatform, float duration)
+    {
+        float time = 0;
+        Vector3 startValue = transform.position;
+        while (time < duration)
+        {
+            anim.SetBool("LedgeClimbing", true);
+            transform.position = Vector3.Lerp(startValue, topOfPlatform, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        ledge = null;
+        moved = false;
+        grabbingLedge = false;
+        anim.SetBool("LedgeClimbing", false);
+    }
+
+    protected virtual void AdjustPlayerPosition()
+    {
+        if (!moved)
+        {
+            moved = true;
+            if (transform.localScale.x > 0)
+            {
+                transform.position = new Vector3(0, (ledge.GetComponent<Collider>().bounds.max.y - col.bounds.extents.y - .5f) + ledge.GetComponent<Ledge>().hangingVerticalOffset, (ledge.GetComponent<Collider>().bounds.min.z - col.bounds.extents.z) + ledge.GetComponent<Ledge>().hangingHorizontalOffset);
+            }
+            else
+            {
+                transform.position = new Vector3(0, (ledge.GetComponent<Collider>().bounds.max.y - col.bounds.extents.y - .5f) + ledge.GetComponent<Ledge>().hangingVerticalOffset, (ledge.GetComponent<Collider>().bounds.max.z + col.bounds.extents.z) - ledge.GetComponent<Ledge>().hangingHorizontalOffset);
+            }
+        }
+    }
+
+    protected virtual void NotFalling()
+    {
+        falling = false;
+    }
+}
