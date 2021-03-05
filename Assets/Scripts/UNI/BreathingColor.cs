1e626eb9eac1c069fb2cd1feb804ed69a7ac7ed2
louis <louis.bumbolo@hotmail.com>
2021-03-05 13:47:20 +0100

    Beginning Breathing ambiance

---
+++ Assets/Scripts/UNI/BreathingColor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreathingColor : MonoBehaviour
+{
+    // UNI
+    private GameObject uni;
+
+    // Beathing mecanic
+    private Breathing_mechanic bm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        // Get uni
+        uni = GameObject.FindGameObjectWithTag("uni");
+
+        // Get uni
+        bm = uni.GetComponent<Breathing_mechanic>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
