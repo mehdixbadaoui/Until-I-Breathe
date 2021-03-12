f06b906299644cc0481d43bd19a2b97d9e1017fa
Leonard <leonard.louis@cpe.fr>
2021-03-12 15:32:40 +0100

    Reworked the way events are handled
    voila voila trop compliqué à expliquer

---
+++ Assets/CancelScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class CancelScript : MonoBehaviour
+{
+    public KeyCode CancelKeyCode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(CancelKeyCode))
+            ExecuteEvents.Execute(gameObject, new BaseEventData(EventSystem.current), ExecuteEvents.cancelHandler);
+    }
+}
