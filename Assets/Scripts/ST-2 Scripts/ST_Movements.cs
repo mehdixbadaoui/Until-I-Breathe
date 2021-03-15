70094e94925dfb4c9cf71fda265dc3b130bd2fc3
Nadogeto <nadjetobbal@gmail.com>
2021-03-15 17:31:52 +0100

    ST-2 dist from hook fixed
    Added params (fixationSpeed, distFromObj and lookAtSpeed)
    Appropriate prefabs on MY scene
    Appropriate camera view on MY scene

--- Assets/Scripts/ST-2 Scripts/ST_Movements.cs
+++ Assets/Scripts/ST-2 Scripts/ST_Movements.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,14 +15,21 @@ public class ST_Movements : MonoBehaviour
 
     public Vector3 rotationOffset;
 
+    [Range(0.2f, 10f)]
+    public float fixationSpeed;
+    [Range(0.5f, 10f)]
+    public float distFromObj;
+    [Range(1f, 10f)]
+    public float lookAtSpeed;
+
     private hook_detector HookDetector;
 
     // To access the children components of ST-2 (sprite)
-    public GameObject ChildGO_Sprite;
-    private Vector3 rotation_Sprite;
-    public Sprite[] sprites;
+    //public GameObject ChildGO_Sprite;
+    //private Vector3 rotation_Sprite;
+    //public Sprite[] sprites;
 
-    public float dist_from_hook = 2f;
+
 
     void Start()
     {
@@ -29,17 +37,14 @@ public class ST_Movements : MonoBehaviour
         Player = GameObject.FindGameObjectWithTag("uni").transform;
 
         // Stores the initial rotation of the sprite component
-        rotation_Sprite = ChildGO_Sprite.transform.rotation.eulerAngles;
-
-        // Stores the initial rotation of the sprite component
-        rotation_Sprite = ChildGO_Sprite.transform.rotation.eulerAngles;
+        //rotation_Sprite = ChildGO_Sprite.transform.rotation.eulerAngles;
 
         Vector3 rotationMask = new Vector3(0, 1, 0);
 
-        // Initiliaze the start position of ST-2
+        // Initialize the start position of ST-2
         Vector3 startOffset = new Vector3(-2.5f, 2f, 0.0f);
 
-        // Initiliaze the position of ST-2 at the start of  the game
+        // Initialize the position of ST-2 at the start of  the game
         transform.position = Player.position + startOffset;
 
         // Cache the initial offset at time of load/spawn
@@ -52,13 +57,14 @@ public class ST_Movements : MonoBehaviour
     private void Update()
     {
         // Prevents the sprite from rotating on the Z axis
-        rotation_Sprite.z = Mathf.Clamp(rotation_Sprite.z, 0f, 0f);
+        //rotation_Sprite.z = Mathf.Clamp(rotation_Sprite.z, 0f, 0f);
         //ChildGO_Sprite.transform.rotation = Quaternion.Euler(rotation_Sprite);
     }
 
     void FixedUpdate()
     {
         // Allows ST-2 to follow the player
+        if (!HookDetector.nearHook && !HookDetector.nearDead && !HookDetector.nearHint)
         {
             // Resets expression
             //ChildGO_Sprite.GetComponent<SpriteRenderer>().sprite = sprites[0];
@@ -79,23 +85,28 @@ public class ST_Movements : MonoBehaviour
 
             // Smooth rotation
             // get a rotation that points Z axis forward, and the Y axis towards the target
-            Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, (Player.position - transform.position ));
+            Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, (Player.position - transform.position));
             // rotate toward the target rotation, never rotating farther than "rotationSpeed" in one frame.
             //transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed);
 
-
             transform.LookAt(Player.Find("ST2 Follow"));
         }
-
-        if (HookDetector.nearHook && !HookDetector.nearDead && !HookDetector.nearHint) // Allows ST-2 to show the nearest HOOK to the player
+        
+        // Allows ST-2 to show the nearest HOOK to the player
+        if (HookDetector.nearHook && !HookDetector.nearDead && !HookDetector.nearHint) 
         {
             // Changes expression
             //ChildGO_Sprite.GetComponent<SpriteRenderer>().sprite = sprites[1];
-
-            Vector3 desiredPosition = (HookDetector.nearest_hook.transform.position + (transform.position - HookDetector.nearest_hook.transform.position).normalized) * dist_from_hook;
-            // Smoothes the path between the initial and desired position
-            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, followSharpness);
+            
+            Vector3 desiredPosition = HookDetector.nearest_hook.transform.position + (transform.position - HookDetector.nearest_hook.transform.position).normalized * distFromObj;
+            // Smooths the path between the initial and desired position
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, fixationSpeed * Time.deltaTime); //hard coded for test, not the best practice
             transform.position = smoothedPosition;
+
+            if (Math.Abs(transform.position.z - desiredPosition.z) < 1.5f) //check if ST2 is near enough from the hook and if so look at player otherwise it means it's still heading there
+            {
+                SmoothLookAt(Player.Find("ST2 Follow").transform.position);
+            }
 
             // Rotates near the object of interest
             //transform.RotateAround(HookDetector.nearest_hook.transform.position, Vector3.right, 180.0f * Time.deltaTime);
@@ -104,10 +115,15 @@ public class ST_Movements : MonoBehaviour
         // Allows ST-2 to show the nearest HINT to the player
         if (!HookDetector.nearHook && !HookDetector.nearDead && HookDetector.nearHint)
         {
-            Vector3 desiredPosition = (HookDetector.hintPosition.position + (transform.position - HookDetector.hintPosition.position).normalized * 2f);
-            // Smoothes the path between the initial and desired position
-            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, followSharpness);
+            Vector3 desiredPosition = (HookDetector.hintPosition.position + (transform.position - HookDetector.hintPosition.position).normalized * distFromObj);
+            // Smooths the path between the initial and desired position
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, fixationSpeed * Time.deltaTime);
             transform.position = smoothedPosition;
+
+            if (Math.Abs(transform.position.z - desiredPosition.z) < 1f) //check if ST2 is near enough from the hook and if so look at player otherwise it means it's still heading there
+            {
+                SmoothLookAt(Player.Find("ST2 Follow").transform.position);
+            }
 
             // Rotates near the object of interest
             //transform.RotateAround(HookDetector.hintPosition.position, Vector3.up, 180.0f * Time.deltaTime);
@@ -116,15 +132,28 @@ public class ST_Movements : MonoBehaviour
         // Allows ST-2 to collect lore from dead robots nearby
         if (!HookDetector.nearHook && !HookDetector.nearHint && HookDetector.nearDead)
         {
-            Vector3 desiredPosition = (HookDetector.deadPosition.position + (transform.position - HookDetector.deadPosition.position).normalized * 1f);
-            // Smoothes the path between the initial and desired position
-            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, followSharpness);
-            // Rotates near the object of interest
+            Vector3 desiredPosition = (HookDetector.deadPosition.position + (transform.position - HookDetector.deadPosition.position).normalized * distFromObj);
+            // Smooths the path between the initial and desired position
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, fixationSpeed * Time.deltaTime);
             transform.position = smoothedPosition;
+
+            if (Math.Abs(transform.position.z - desiredPosition.z) < 1f) //check if ST2 is near enough from the hook and if so look at player otherwise it means it's still heading there
+            {
+                SmoothLookAt(Player.Find("ST2 Follow").transform.position);
+            }
+
+            // Rotates near the object of interest
+            //transform.RotateAround(HookDetector.hintPosition.position, Vector3.up, 180.0f * Time.deltaTime);
 
             // Add features relative to HUD of lore found on dead robots and change expression to something sad/confused??
             //ChildGO_Sprite.GetComponent<SpriteRenderer>().sprite = sprites[2];
         }
+    }
+
+    //To look at the player smoothly after heading to a different location (different than the initial offset)
+    void SmoothLookAt(Vector3 newDirection)
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(newDirection - transform.position), lookAtSpeed * Time.deltaTime);
     }
 
 }
