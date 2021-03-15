5c0925f2769bba070c0edc5c31959295facce8fe
Mehdi Badaoui <mehdixbadaoui@outlook.fr>
2021-03-15 16:57:41 +0100

    changed camera zoom (need to work on lerp)

---
+++ Assets/Scripts/Environment/CameraZoom.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    public Vector3 zoom;
+    private CameraFollow cameraFollow;
+    private float initialSpped;
+    public float zoom_speed = 0f;
+    private Vector3 cameraInitialPos;
+    private Transform cameraZoom;
+    GameObject cameraFollow_obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraFollow = FindObjectOfType<CameraFollow>();
+        initialSpped = cameraFollow.smoothSpeed;
+        cameraInitialPos = cameraFollow.transform.position;
+        cameraZoom = transform.Find("Camera Zoom");
+        cameraFollow_obj = GameObject.Find("Camera Follow");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("uni"))
+        {
+            if(transform.Find("Camera Zoom"))
+            {
+                cameraFollow.zoom_offset = Vector3.Lerp(cameraFollow.zoom_offset, zoom, zoom_speed);
+                cameraZoom.position = Vector3.Lerp(cameraZoom.position, new Vector3(cameraFollow_obj.transform.position.x, cameraZoom.position.y, cameraFollow_obj.transform.position.z) + cameraFollow.zoom_offset, zoom_speed);
+
+                cameraFollow.player = cameraZoom; 
+            }
+            else
+            {
+                cameraFollow.player = GameObject.Find("Camera Follow").transform;
+                cameraFollow.zoom_offset = Vector3.Lerp(cameraFollow.zoom_offset, zoom, zoom_speed);
+            }
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("uni"))
+        {
+            StartCoroutine(ResetCamera());
+            cameraFollow.player = GameObject.Find("Camera Follow").transform;
+        }
+
+
+    }
+
+    IEnumerator ResetCamera()
+    {
+        float startTime = Time.time;
+        while (Time.time < startTime + zoom_speed)
+        {
+            cameraFollow.zoom_offset = Vector3.Lerp(cameraFollow.zoom_offset, Vector3.zero,  2 * (Time.time - startTime) / zoom_speed);
+            yield return null;
+        }
+        cameraFollow.zoom_offset = Vector3.zero;
+    }
+
+}
