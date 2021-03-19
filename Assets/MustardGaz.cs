71fdb14f4da94438e8bb7aa596b7ac13df209187
Mehdi Badaoui <mehdixbadaoui@outlook.fr>
2021-03-19 14:23:53 +0100

    Revert "Update MustardGaz"
    This reverts commit 2156af928b5d5a63a0cb03a2c8a189be810d97e7.

--- Assets/MustardGaz.cs
+++ Assets/MustardGaz.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class MustardGaz : MonoBehaviour
 {
-    public bool CanHold = true;
+    private float initial_breath_speed;
     // Start is called before the first frame update
     void Start()
     {
+        initial_breath_speed = FindObjectOfType<Breathing_mechanic>().breath_speed;
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("uni") && !other.GetComponent<Breathing_mechanic>().hold && CanHold)
+        if (other.CompareTag("uni") && !other.GetComponent<Breathing_mechanic>().hold)
         {
             GetComponentInParent<CanBreathe>().enabled = false;
             other.GetComponent<Breathing_mechanic>().can_breath = false;
