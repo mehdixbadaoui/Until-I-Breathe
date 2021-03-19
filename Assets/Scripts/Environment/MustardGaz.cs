2156af928b5d5a63a0cb03a2c8a189be810d97e7
Mehdi Badaoui <mehdixbadaoui@outlook.fr>
2021-03-19 14:21:12 +0100

    Update MustardGaz

--- Assets/Scripts/Environment/MustardGaz.cs
+++ Assets/Scripts/Environment/MustardGaz.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class MustardGaz : MonoBehaviour
 {
-    private float initial_breath_speed;
+    public bool CanHold = true;
     // Start is called before the first frame update
     void Start()
     {
-        initial_breath_speed = FindObjectOfType<Breathing_mechanic>().breath_speed;
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("uni") && !other.GetComponent<Breathing_mechanic>().hold)
+        if (other.CompareTag("uni") && !other.GetComponent<Breathing_mechanic>().hold && CanHold)
         {
             GetComponentInParent<CanBreathe>().enabled = false;
             other.GetComponent<Breathing_mechanic>().can_breath = false;
