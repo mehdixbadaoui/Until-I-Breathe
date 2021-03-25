9a705ae955349224e65b5dc1fba0e868d71df4b7
Leonard <leonard.louis@cpe.fr>
2021-03-25 17:01:22 +0100

    Rework of pause menu working from now on
    Still got things to do but it is avanced now

--- Assets/UI/PauseMenu.cs
+++ Assets/UI/PauseMenu.cs
@@ -1,36 +1,78 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
-    public UnityEvent Resume;
-    public UnityEvent Quit;
+    public Text SelectionText;
 
-    // Start is called before the first frame update
-    void Start()
+    public GameObject MainTab;
+    private GameObject currentTab;
+    private List<GameObject> previousTabs;
+
+    [Serializable]
+    public struct PauseMenuButtonData
     {
+        public string Name;
+        public RectTransform SelectionTransform;
+        public UnityEvent Event;
+    }
+    public List<PauseMenuButtonData> ButtonsData;
 
+    private void Awake()
+    {
+        previousTabs = new List<GameObject>();
+
+        currentTab = MainTab;
+        currentTab.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Escape))
+            CloseCurrentTab();
     }
 
-    public void TriggerResume()
+    public void Select(int i)
     {
-        Debug.Log("Invoking resume events");
-
-        Resume.Invoke();
+        SelectionText.text = "< " + ButtonsData[i].Name + " >";
+        ButtonsData[i].SelectionTransform.gameObject.SetActive(true);
+    }
+    public void Deselect(int i)
+    {
+        SelectionText.text = "";
+        ButtonsData[i].SelectionTransform.gameObject.SetActive(false);
+    }
+    public void Submit(int i)
+    {
+        ButtonsData[i].Event.Invoke();
     }
 
-    public void TriggerQuit()
+    public void OpenTab(GameObject tab)
     {
-        Debug.Log("Invoking quit events");
+        currentTab.SetActive(false);
 
-        Quit.Invoke();
+        previousTabs.Add(currentTab);
+        currentTab = tab;
+
+        currentTab.SetActive(true);
+    }
+    public void CloseCurrentTab()
+    {
+        if (previousTabs.Count == 0)
+            return;
+        else
+        {
+            currentTab.SetActive(false);
+
+            currentTab = previousTabs[previousTabs.Count - 1];
+            previousTabs.RemoveAt(previousTabs.Count - 1);
+
+            currentTab.SetActive(true);
+        }
     }
 }
---
+++ Assets/pauseMenuButton.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseMenuButton : MonoBehaviour
+{
+    public string SelectionText;
+    public RectTransform SelectionPlaceHolder;
+}
