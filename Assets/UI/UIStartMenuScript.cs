7e0a874883af7316d2507fca9d4339c2b0cba806
Leonard <leonard.louis@cpe.fr>
2021-03-25 11:53:35 +0100

    plein de trucs
    on s'en tape gros ca marche meme plus

--- Assets/UI/UIStartMenuScript.cs
+++ Assets/UI/UIStartMenuScript.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class UIStartMenuScript : MonoBehaviour
 {
@@ -17,6 +18,9 @@ public class UIStartMenuScript : MonoBehaviour
     public Color BaseColor;
     public Color HighlightedColor;
     private GameObject highlightedGO;
+
+    [SerializeField]
+    private InputActionAsset iaa;
 
     // Start is called before the first frame update
     void Start()
