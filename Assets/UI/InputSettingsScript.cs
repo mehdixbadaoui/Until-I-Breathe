b8d7b65e32eb0560c0ec0a2c5963facbd405153d
Leonard Louis <Kronro.Sihao@gmail.com>
2021-03-30 09:54:44 +0200

    Added things to the input settings
    started to work on the change input feature

--- Assets/UI/InputSettingsScript.cs
+++ Assets/UI/InputSettingsScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -12,6 +13,10 @@ public class InputSettingsScript : MonoBehaviour
     public GameObject Group;
     public GameObject Cell;
 
+    private List<InputAction> InputActionList;
+    private InputAction _actionToChange;
+    private int _bindingIndex;
+
     void Start()
     {
         var map = InputActionAsset.actionMaps;
@@ -20,18 +25,56 @@ public class InputSettingsScript : MonoBehaviour
         {
             foreach (var action in item.actions)
             {
-                var cell = Instantiate(Cell, Group.transform);
+                int bindingIndex = 0;
+                foreach(var binding in action.bindings)
+                {
+                    var cell = Instantiate(Cell, Group.transform);
 
-                cell.transform.GetChild(0).GetComponent<Text>().text = action.name;
-                cell.transform.GetChild(1).GetComponentInChildren<Text>().text = action.bindings[0].path;           
+                    cell.transform.GetChild(0).GetComponent<Text>().text = action.name;
+                    cell.transform.GetChild(1).GetComponent<Button>().onClick.AddListener(delegate { ChangeBinding(action, bindingIndex); });
+                    cell.transform.GetChild(1).GetComponentInChildren<Text>().text = binding.path;
 
-                cell.name = action.name;
+                    cell.name = action.name;
+
+                    bindingIndex++;
+                }
             }
         }
     }
 
-    void Update()
+    private void MakeIAAFromList(InputActionMap[] iamList, Dictionary<InputActionMap, InputAction[]> actionMap)
+    {
+        InputActionAsset iaa = new InputActionAsset();
+
+        foreach(var iam in iamList)
+        {
+        }
+    }
+
+    private void ChangeBinding(InputAction action, int bindingIndex)
+    {
+        _actionToChange = action;
+        _bindingIndex = bindingIndex;
+    }
+
+    private void Update()
     {
         
+    }
+
+    private void OnGUI()
+    {
+        if (_actionToChange != null)
+        {
+            Event e = Event.current;
+            if (e.isKey)
+            {
+                if (e.keyCode != KeyCode.None)
+                {
+                    Debug.Log((int)e.keyCode);
+
+                }
+            }
+        }
     }
 }
