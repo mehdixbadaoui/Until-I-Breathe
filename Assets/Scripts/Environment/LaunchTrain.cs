99e0a7acb09efec795204255ea9569e88960a144
Mehdi Badaoui <mehdixbadaoui@outlook.fr>
2021-04-01 16:46:00 +0200

    launching train works (need to work on speed build up)

--- Assets/Scripts/Environment/LaunchTrain.cs
+++ Assets/Scripts/Environment/LaunchTrain.cs
@@ -13,11 +13,32 @@ public class LaunchTrain : MonoBehaviour
             BezierSpline track_bz = Track.GetComponent<BezierSpline>();
             Locomotive.GetComponent<SplineWalker>().spline = track_bz;
             Locomotive.GetComponent<SplineWalker>().progress = 0;
+            Locomotive.GetComponent<SplineWalker>().speed = 0;
+            StartCoroutine(SlowStart());
 
             //if (Locomotive.GetComponent<SplineWalker>().lookForward)
             //{
             //    transform.LookAt(Locomotive.transform.localPosition + track_bz.GetDirection(Locomotive.GetComponent<SplineWalker>().progress));
             //}
         }
+    }
+
+    IEnumerator SlowStart()
+    {
+        float speed = Locomotive.GetComponent<SplineWalker>().speed;
+
+        float time = 0;
+        float waitTime = 3f;
+
+        while (time < waitTime && Locomotive.GetComponent<SplineWalker>().speed < 20)
+        {
+            Locomotive.GetComponent<SplineWalker>().speed = Mathf.Lerp(0/*Locomotive.GetComponent<SplineWalker>().speed*/, 20, time / waitTime);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        Locomotive.GetComponent<SplineWalker>().speed = 20;
+        yield return null;
+
+
     }
 }
--- Assets/Spline Walker Train/SplineWalker.cs
+++ Assets/Spline Walker Train/SplineWalker.cs
@@ -4,18 +4,19 @@ public class SplineWalker : MonoBehaviour {
 
 	public BezierSpline spline;
 
-	public float duration;
-
 	public bool lookForward;
+
+	public float speed = 1f;
 
 	public SplineWalkerMode mode;
 
-	public float progress;
+	[HideInInspector] public float progress;
 	private bool goingForward = true;
 
 	private void Update () {
 		if (goingForward) {
-			progress += (Time.deltaTime / duration);
+			//progress += (Time.deltaTime / duration);
+			progress += (Time.deltaTime * speed / 100);
 			if (progress > 1f) {
 				if (mode == SplineWalkerMode.Once) {
 					progress = 1f;
@@ -30,15 +31,19 @@ public class SplineWalker : MonoBehaviour {
 			}
 		}
 		else {
-			progress -= Time.deltaTime / duration;
+			progress -= Time.deltaTime * speed / 100;
 			if (progress < 0f) {
 				progress = - progress;
 				goingForward = true;
 			}
 		}
 
-		Vector3 position = spline.GetPoint(progress);
+        //Vector3 position = spline.GetPoint(progress);
+        //transform.localPosition = position;
+
+        Vector3 position = Vector3.MoveTowards(transform.localPosition, spline.GetPoint(progress), speed);
 		transform.localPosition = position;
+
 		if (lookForward) {
 			transform.LookAt(position + spline.GetDirection(progress));
 		}
