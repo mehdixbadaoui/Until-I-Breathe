534ed844954f582109a8b68c91183a7e526ff553
Mehdi <mehdixbadaoui@outlook.fr>
2021-04-02 16:59:30 +0200

    Rising fog

---
+++ Assets/Scripts/Environment/RisingFog.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RisingFog : MonoBehaviour
+{
+    public float speed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, Mathf.Lerp(0, 1, .5f), 0) * speed / 10);
+    }
+}
