cbf9632ceedbcb29b2323bf38ffccd6a304179cc
Mehdi <mehdixbadaoui@outlook.fr>
2021-04-02 18:00:06 +0200

    Poison gaz kills

--- Assets/Scripts/Environment/RisingFog.cs
+++ Assets/Scripts/Environment/RisingFog.cs
@@ -4,11 +4,31 @@ using UnityEngine;
 
 public class RisingFog : MonoBehaviour
 {
-    public float speed = 5f;
-
+    public CanBreathe SafeZone;
+    public float RiseSpeed = 5f;
+    public float KillSpeed = 5f;
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(0, Mathf.Lerp(0, 1, .5f), 0) * speed / 10);
+        //transform.Translate(new Vector3(0, Mathf.Lerp(0, 1, .5f), 0) * RiseSpeed / 10);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("uni"))
+        {
+            SafeZone.enabled = false;
+            other.GetComponent<Breathing_mechanic>().can_breath = false;
+            other.GetComponent<Breathing_mechanic>().breath -= KillSpeed * Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("uni"))
+        {
+            SafeZone.enabled = true;
+        }
+
     }
 }
