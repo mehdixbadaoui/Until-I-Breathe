6ece2ceb4b6e3a6e7df1d13675e750af2db445b6
Tristan <tristan.glandut@gmail.com>
2021-04-06 12:03:00 +0200

    Update 1-1 + 3-1

---
+++ Assets/Tristan/Anim/StartAnimVent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAnimVent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator animController;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GetComponent<Fan>().isOn == true)
+        {
+            animController.Play("AnimVentTuto");
+        }
+    }
+}
