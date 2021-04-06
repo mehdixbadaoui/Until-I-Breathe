85751d28e57687eb434f8f2783ca3557754e8bf4
Mehdi Badaoui <mehdixbadaoui@outlook.fr>
2021-04-06 15:10:33 +0200

    balloon keeps rising

--- Assets/Scripts/Environment/ballon.cs
+++ Assets/Scripts/Environment/ballon.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class ballon : MonoBehaviour
 {
-    [SerializeField] private float air;
+    public float air;
     public float capacity;
-    Vector3 initial_position, target_position;
+    public float force = 1f;
+    //Vector3 initial_position, target_position;
 
     public float travel_dist;
     public float travel_speed;
     // Start is called before the first frame update
     void Start()
     {
-        air = 0;
-        initial_position = transform.position;
-        target_position = initial_position + Vector3.up * travel_dist;
+        //air = 0;
+        //initial_position = transform.position;
+        //target_position = initial_position + Vector3.up * travel_dist;
 
     }
 
@@ -24,7 +25,9 @@ public class ballon : MonoBehaviour
     {
         if(air >= capacity)
         {
-            transform.position = Vector3.Lerp(transform.position, target_position, travel_speed);
+            //transform.position = Vector3.MoveTowards(transform.position, target_position, travel_speed);
+            transform.Translate(Vector3.up * travel_speed / 10);
+            //transform.
         }
     }
 
