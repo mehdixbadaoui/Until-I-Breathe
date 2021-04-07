6ecd4225d6e7be5edf6cd7700fce09a4a70f31a5
Nadogeto <nadjetobbal@gmail.com>
2021-04-07 15:29:44 +0200

    Script to hide and show mesh of an object onTrigger

---
+++ Assets/Scripts/Environment/TriggerMesh.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerMesh : MonoBehaviour
+{
+    public GameObject Object;
+
+    MeshRenderer MeshOfObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MeshOfObject = Object.GetComponent<MeshRenderer>();    
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("uni"))
+        {
+            MeshOfObject.enabled = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("uni"))
+        {
+            MeshOfObject.enabled = true;
+        }
+    }
+}
