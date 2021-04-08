497bf46694e55bbfdae5b1e5a98e40b101253edd
Tristan <tristan.glandut@gmail.com>
2021-04-08 10:52:53 +0200

    Modif Script Patrol drones to make it work

--- Assets/Scripts/Environment/Drones/PatrolDrones.cs
+++ Assets/Scripts/Environment/Drones/PatrolDrones.cs
@@ -68,7 +68,12 @@ public class PatrolDrones : MonoBehaviour
 
         int targetWaypointIndex = 1;
         Vector3 targetWaypoint = waypoints[targetWaypointIndex];
-        transform.LookAt(targetWaypoint);
+
+        if(canTurn)
+        {
+            transform.LookAt(targetWaypoint);
+        }
+            
 
         while (true)
         {
@@ -101,16 +106,16 @@ public class PatrolDrones : MonoBehaviour
 
     IEnumerator CallShootWithDelay()
     {
-        muzzleFlash.Play();
-        GameObject impactGO = Instantiate(impactEffect, player.position, Quaternion.identity);
-        Destroy(impactGO, 1f);
+        //muzzleFlash.Play();
+        //GameObject impactGO = Instantiate(impactEffect, player.position, Quaternion.identity);
+        //Destroy(impactGO, 1f);
         yield return new WaitForSeconds(timeToKillPlayer);
         GM.Die();
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") || other.CompareTag("uni"))
         {
             Debug.DrawLine(transform.position, player.position);
             if (!Physics.Linecast(transform.position, player.position, viewMask))
