367e7bda3c393816a30e0e5be0ece3674fd28875
Mehdi Badaoui <mehdixbadaoui@outlook.fr>
2021-04-08 12:04:26 +0200

    nacelle goes up

--- Assets/Scripts/Environment/Nacelle.cs
+++ Assets/Scripts/Environment/Nacelle.cs
@@ -4,21 +4,40 @@ using UnityEngine;
 
 public class Nacelle : MonoBehaviour
 {
-    public Transform target;
+    public float distance;
     public float WaitTime;
-    public float travelTime;
+    public float speed;
+
+    float target; 
+    float time;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("uni"))
         {
-            StartCoroutine(GoUp());
+            //StartCoroutine(GoUp());
+            target = transform.position.y + distance;
+            time = 0;
         }
     }
 
-    IEnumerator GoUp()
+    private void OnTriggerStay(Collider other)
     {
-        yield return new WaitForSeconds(WaitTime);
-        transform.position = Vector3.MoveTowards(transform.position, target.position, travelTime);
+        if (other.CompareTag("uni"))
+        {
+            if (time < WaitTime)
+                time += Time.deltaTime;
+            //StartCoroutine(GoUp());
+            if (transform.position.y < distance && time >= WaitTime)
+                transform.position += Vector3.up * speed * Time.deltaTime;
+
+        }
+
     }
+
+    //IEnumerator GoUp()
+    //{
+    //    yield return new WaitForSeconds(WaitTime);
+    //    transform.position = Vector3.MoveTowards(transform.position, target.position, travelTime);
+    //}
 }
