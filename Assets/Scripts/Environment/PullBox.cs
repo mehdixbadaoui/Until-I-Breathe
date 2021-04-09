33c3b7cc4a494cbdb2d722047374f0f191464091
Mehdi Badaoui <mehdixbadaoui@outlook.fr>
2021-04-09 15:14:48 +0200

    pull box works 90%

---
+++ Assets/Scripts/Environment/PullBox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullBox : MonoBehaviour
+{
+    public GameObject box;
+    public float force = 4f;
+
+    private void Start()
+    {
+
+    }
+
+    public void Pull()
+    {
+        Debug.Log("puuuuulllll");
+        GetComponent<Rigidbody>().isKinematic = false;
+        box.GetComponent<Rigidbody>().AddForce(force, 0, 0);
+
+    }
+}
