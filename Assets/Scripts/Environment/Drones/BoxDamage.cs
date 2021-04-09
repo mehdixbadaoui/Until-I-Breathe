e27f0abf1284b5a06805aea543ecd982b71751d5
Mehdi Badaoui <mehdixbadaoui@outlook.fr>
2021-04-09 13:03:15 +0200

    turn off lights

--- Assets/Scripts/Environment/Drones/BoxDamage.cs
+++ Assets/Scripts/Environment/Drones/BoxDamage.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class BoxDamage : MonoBehaviour
 {
+    public List<Light> Lights;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.CompareTag("box"))
         {
             GetComponent<Rigidbody>().isKinematic = false;
+            foreach(Light light in Lights)
+            {
+                light.intensity = 0;
+            }
         }
     }
 }
