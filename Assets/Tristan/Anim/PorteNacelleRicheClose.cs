37d6048ceeb37abe77fe08649dd1cb84d41a505d
Tristan <tristan.glandut@gmail.com>
2021-04-12 16:36:32 +0200

    Lots of modifs in 3-1

---
+++ Assets/Tristan/Anim/PorteNacelleRicheClose.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAnim : MonoBehaviour
+{
+
+    public Animator anim;
+    public TriggerAnim myTr;
+    private GameObject uni;
+    private PlayEventSounds playEvent; 
+    public string animName;
+
+    // Start is called before the first frame update
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+        uni = GameObject.FindGameObjectWithTag("uni");
+        playEvent = uni.GetComponent<PlayEventSounds>(); 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (myTr.inside == true)
+         anim.Play(animName);
+    }
+}
