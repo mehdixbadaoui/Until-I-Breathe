3e4c010846d0140245605b92c9e871fa0895ca89
Benjamin Patoizeau <benjamin.patoizeau@cpe.fr>
2021-04-30 12:27:15 +0200

    Dialogue Sounds ST2 + engin volant son plus fort

--- Assets/Scripts/Environment/Dialogue.cs
+++ Assets/Scripts/Environment/Dialogue.cs
@@ -17,11 +17,67 @@ public class Dialogue : MonoBehaviour
 
     public Canvas canvas;
 
+    //ST2_Sound
+    public bool doOnce;
+    public bool ST2_Voice;
+    public bool ST2_sad;
+    public bool ST2_normal;
+    public bool ST2_danger;
+    private bool doEveryTime;
+    private PlayEventSounds playEventSounds; 
+
 
     private void Start()
     {
+        doEveryTime = !doOnce;
+        playEventSounds = GameObject.FindGameObjectWithTag("uni").GetComponent<PlayEventSounds>();
         canvas.gameObject.GetComponent<CanvasGroup>().alpha = 0;
         
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("uni"))
+        {
+            if(doOnce)
+            {
+                if(ST2_Voice)
+                {
+                    if (ST2_normal)
+                    {
+                        playEventSounds.PlayEventWithoutRTPC("ST2_normal_event", this.gameObject);
+
+                    }
+                    else if (ST2_sad)
+                    {
+                        playEventSounds.PlayEventWithoutRTPC("ST2_triste_event", this.gameObject);
+                    }
+                    else if (ST2_danger)
+                    {
+                        playEventSounds.PlayEventWithoutRTPC("ST2_danger_event", this.gameObject);
+                    }
+                }
+                doOnce = false; 
+            }
+            else if(doEveryTime)
+            {
+                if (ST2_Voice)
+                {
+                    if (ST2_normal)
+                    {
+                        playEventSounds.PlayEventWithoutRTPC("ST2_normal_event", this.gameObject);
+
+                    }
+                    else if (ST2_sad)
+                    {
+                        playEventSounds.PlayEventWithoutRTPC("ST2_triste_event", this.gameObject);
+                    }
+                    else if (ST2_danger)
+                    {
+                        playEventSounds.PlayEventWithoutRTPC("ST2_danger_event", this.gameObject);
+                    }
+                }
+            }
+        }
     }
     private void OnTriggerStay(Collider other)
     {
