e8b78dc37860f6a4b3b2356426ec7f23b4d81f3b
Amphinobi <47356076+Amphinobi@users.noreply.github.com>
2021-05-04 14:00:27 +0200

    S1L3 Ventillo fixed

---
+++ Assets/Scripts/Environment/DistanceForVentilator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceForVentilator : MonoBehaviour
+{
+    private GameObject uni;
+    private GameObject ventilator;
+    public Vector3 distanceVentilatorUni;
+    public float maxDistanceFromVentilator = 15f;
+    private PlayEventSounds playEventWithRTPCSound;
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uni = GameObject.FindGameObjectWithTag("uni");
+        playEventWithRTPCSound = uni.GetComponent<PlayEventSounds>();
+
+        ventilator = this.gameObject;
+        ventilator.GetComponent<VentilatorSound>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    private void FixedUpdate()
+    {
+        distanceVentilatorUni = playEventWithRTPCSound.CalculateDistanceUniFromObject(this.transform.position);
+
+        if (Mathf.Abs(distanceVentilatorUni.z) <= maxDistanceFromVentilator && Mathf.Abs(distanceVentilatorUni.y) <= maxDistanceFromVentilator && Mathf.Abs(distanceVentilatorUni.x) <= maxDistanceFromVentilator)
+        {
+            ventilator.GetComponent<VentilatorSound>().enabled = true;
+        }
+        else
+        {
+            ventilator.GetComponent<VentilatorSound>().enabled = false;
+        }
+       
+
+
+    }
+}
